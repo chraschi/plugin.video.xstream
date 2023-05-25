da9a37e227cf1939532df3752a00a31ba0d27dbc
DWH-WFC <45105289+DWH-WFC@users.noreply.github.com>
2023-05-25 21:45:51 +0200

    [Cinemathek] komplett überarbeitet

--- sites/cinemathek.py
+++ sites/cinemathek.py
@@ -2,14 +2,16 @@
 # Python 3
 # Always pay attention to the translations in the menu!
 # HTML LangzeitCache hinzugefügt
+    #showGenre:     48 Stunden
     #showEntries:    6 Stunden
+    #showSeasons:    6 Stunden
     #showEpisodes:   4 Stunden
     
 import json
 
 from resources.lib.handler.ParameterHandler import ParameterHandler
 from resources.lib.handler.requestHandler import cRequestHandler
-from resources.lib.tools import logger, cParser, cUtil
+from resources.lib.tools import logger, cParser
 from resources.lib.gui.guiElement import cGuiElement
 from resources.lib.config import cConfig
 from resources.lib.gui.gui import cGui
@@ -18,7 +20,7 @@ SITE_IDENTIFIER = 'cinemathek'
 SITE_NAME = 'Cinemathek'
 SITE_ICON = 'cinemathek.png'
 
-#Global search function is thus deactivated!
+# Global search function is thus deactivated!
 if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'false':
     SITE_GLOBAL_SEARCH = False
     logger.info('-> [SitePlugin]: globalSearch for %s is deactivated.' % SITE_NAME)
@@ -56,7 +58,7 @@ def showGenre():
     if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'true':
         oRequest.cacheTime = 60 * 60 * 48  # 48 Stunden
     sHtmlContent = oRequest.request()
-    pattern = '<ul class="sub-menu"><li id="menu-item-23517".*?</ul>' # Alle Einträge in dem Bereich
+    pattern = '<ul class="sub-menu"><li id="menu-item-23517".*?</ul>' # Alle Einträge in dem Bereich suchen
     isMatch, sHtmlContainer = cParser.parseSingleResult(sHtmlContent, pattern)
     if isMatch:
         isMatch, aResult = cParser.parse(sHtmlContainer, 'href="([^"]+).*?>([^<]+)') # sUrl + sName
@@ -113,14 +115,17 @@ def showEntries(entryUrl=False, sGui=False, sSearchText=False):
         isDesc, sDesc = cParser.parseSingleResult(sDummy, '<div class="texto">([^<]+)') # Beschreibung
         if not isDesc:
             isDesc, sDesc = cParser.parseSingleResult(sDummy, 'class="contenido"><p>([^<]+)\s') # Beschreibung in der Suche
+
         isYear, sYear = cParser.parseSingleResult(sDummy, 'class="imdb">\S+.*?\S+.*?<span>([\d]+)') # Release Jahr
         if not isYear:
             isYear, sYear = cParser.parseSingleResult(sDummy, 'class="year">([\d]+)') # Release Jahr in der Suche
+
         isDuration, sDuration = cParser.parseSingleResult(sDummy, '<span class="imdb">\S+.*?\S+.*?\S+.*?\S([\d]+)') # Laufzeit
 
         isRating, sRating = cParser.parseSingleResult(sDummy, 'IMDb:([^<]+)') # IMDb Bewertung
         if not isRating:
             isRating, sRating = cParser.parseSingleResult(sDummy, 'IMDb\s([^<]+)')# IMDb Bewertung in der Suche
+
         oGuiElement = cGuiElement(sName, SITE_IDENTIFIER, 'showSeasons' if isTvshow else 'showHosters')
         oGuiElement.setMediaType('tvshow' if isTvshow else 'movie')
         oGuiElement.setThumbnail(sThumbnail)
@@ -128,12 +133,14 @@ def showEntries(entryUrl=False, sGui=False, sSearchText=False):
             oGuiElement.setDescription(sDesc)
         if isYear:
             oGuiElement.setYear(sYear)
-        if isTvshow is False:
+        if isTvshow is False: # Laufzeit bei Serie ausblenden
             if isDuration:
                 oGuiElement.addItemValue('duration', sDuration)
         if isRating:
             oGuiElement.addItemValue('rating', sRating)
+        # Parameter übergeben
         params.setParam('sThumbnail', sThumbnail)
+        params.setParam('sDesc', sDesc)
         params.setParam('entryUrl', sUrl)
         params.setParam('sName', sName)
         oGui.addFolder(oGuiElement, params, isTvshow, total)
@@ -150,44 +157,52 @@ def showEntries(entryUrl=False, sGui=False, sSearchText=False):
 
 def showSeasons():
     params = ParameterHandler()
+    # Parameter laden
     entryUrl = params.getValue('entryUrl')
-    sHtmlContent = cRequestHandler(entryUrl).request()
+    sThumbnail = params.getValue('sThumbnail')
+    sDesc = params.getValue('sDesc')
+    oRequest = cRequestHandler(entryUrl)
+    if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'true':
+        oRequest.cacheTime = 60 * 60 * 6  # HTML Cache Zeit 6 Stunden
+    sHtmlContent = oRequest.request()
     isMatch, aResult = cParser.parse(sHtmlContent, 'Season ([\d]+)') # Sucht den Staffel Eintrag und d fügt die Anzahl hinzu
     if not isMatch:
         cGui().showInfo()
         return
 
-    isDesc, sDesc = cParser.parseSingleResult(sHtmlContent, 'class="wp-content">(.*?)</p>') # Staffel Beschreibung
     total = len(aResult)
     for sSeason in aResult:
         oGuiElement = cGuiElement('Staffel ' + sSeason, SITE_IDENTIFIER, 'showEpisodes')
         oGuiElement.setMediaType('season')
         oGuiElement.setSeason(sSeason)
-        oGuiElement.setThumbnail(params.getValue('sThumbnail'))
-        if isDesc:
-            oGuiElement.setDescription(sDesc)
+        oGuiElement.setThumbnail(sThumbnail)
+        oGuiElement.setDescription(sDesc)
+        # Parameter übergeben
         params.setParam('Season', sSeason)
         cGui().addFolder(oGuiElement, params, True, total)
     cGui().setView('seasons')
     cGui().setEndOfDirectory()
 
 
-def showEpisodes(): # einfache Abfrage wird wenn es mal funktioniert erweitert für sName,sThumb,Desc
+def showEpisodes():
     params = ParameterHandler()
+    # Parameter laden
     entryUrl = params.getValue('entryUrl')
     sSeason = params.getValue('Season')
-    #sHtmlContent = cRequestHandler(entryUrl).request()
     oRequest = cRequestHandler(entryUrl)
     if cConfig().getSetting('global_search_' + SITE_IDENTIFIER) == 'true':
         oRequest.cacheTime = 60 * 60 * 4  # HTML Cache Zeit 4 Stunden
     sHtmlContent = oRequest.request()
-    pattern = '>Season %s <i>.*?</ul>' % sSeason
+    pattern = '>Season %s <i>.*?</ul>' % sSeason # Suche alles in diesem Bereich
     isMatch, sContainer = cParser.parseSingleResult(sHtmlContent, pattern)
     if isMatch:
-        pattern = '''<li class="mark-([\d]+).*?data-lazy-src="([^"]+).*?<a href="([^"]+).*?>([^<]+)'''
+        pattern = '<li class="mark-([\d]+).*?' # Episoden Eintrag
+        pattern += 'data-lazy-src="([^"]+).*?' # sThumbnail
+        pattern += '<a href="([^"]+).*?' # sUrl
+        pattern += '>([^<]+)' # sName
         isMatch, aResult = cParser.parse(sContainer, pattern)
 
-    isDesc, sDesc = cParser.parseSingleResult(sHtmlContent, 'class="wp-content">(.*?)</p>') # Staffel Beschreibung
+    isDesc, sDesc = cParser.parseSingleResult(sHtmlContent, 'class="wp-content">(.*?)</p>')  # Beschreibung
     total = len(aResult)
     for sEpisode, sThumbnail, sUrl, sName in aResult:
         oGuiElement = cGuiElement('Episode ' + sEpisode + ' - ' + sName, SITE_IDENTIFIER, 'showHosters')
@@ -197,6 +212,7 @@ def showEpisodes(): # einfache Abfrage wird wenn es mal funktioniert erweitert f
         oGuiElement.setThumbnail(sThumbnail)
         if isDesc:
             oGuiElement.setDescription(sDesc)
+        # Parameter übergeben
         params.setParam('sName', sName)
         params.setParam('entryUrl', sUrl)
         cGui().addFolder(oGuiElement, params, False, total)
@@ -208,9 +224,12 @@ def showHosters():
     hosters = []
     sUrl = ParameterHandler().getValue('entryUrl')
     sHtmlContent = cRequestHandler(sUrl).request()
-    #pattern = "player-option-\d.*?type.*?'([^']+).*?(\d+).*?(\d)"
-    #pattern = 'player-option-\d.*?type="([^"]+).*?(\d+).*?(\d)'
-    pattern = 'player-option-\d.*?type="([^"]+).*?(\d+).*?(\d).*?starten!\s([^<]+)'
+    # <li id="player-option-1" class="dooplay_player_option" data-type="movie" data-post="29224" data-nume="1">
+    pattern = 'player-option-\d.*?' # Player Option Nr. z.B 1 für Link 1
+    pattern += 'type="([^"]+).*?' # Eintrag [0] movie oder tv
+    pattern += '(\d+).*?' # Eintrag [1] ist die Film oder Serien Nr.29224
+    pattern += '(\d).*?' # Eintrag [2] ist die Link Nr.1
+    pattern += 'starten!\s([^<]+)' # Eintrag [3] = sName des Link Eintrag
     isMatch, aResult = cParser().parse(sHtmlContent, pattern)
     if isMatch:
         for i in aResult:
@@ -224,9 +243,10 @@ def showHosters():
 
 def getHosterUrl(sUrl=False):
     sUrl = json.loads(cRequestHandler(sUrl).request()).get("embed_url")
+    # Überprüfe sUrl auf korrekte Domain
     Request = cRequestHandler(sUrl, caching=False)
     Request.request()
-    sUrl = Request.getRealUrl() # hole reale URL von der Umleitung
+    sUrl = Request.getRealUrl() # hole reale sURL
     return [{'streamUrl': sUrl, 'resolved': False}]
 
 
